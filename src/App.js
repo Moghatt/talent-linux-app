4ad9ec422e254a1e24b138f427749f63b4ed6115
moghatt <mohammad_ghatali_kh@yahoo.com>
2023-09-21 20:31:56 +0930

    initial mvp

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
+import { Navbar } from "./pages/Navbar";
+import { useAppContext } from "./context/appContext";
+
+function App() {
+  const {showDark, showDarkClass,dispatch} = useAppContext()
+  const handleDarkMode = ()=> {
+      
+    if(!showDark){
+      dispatch({ type: "SHOW_DARK_MODE" }); 
+    
+     
+    }else{
+      dispatch({type:"HIDE_DARK_MODE"})
+    }
+
+
+  }
+    return (
+        <>
+            <body className={`${showDarkClass}`}>
+                <div className={`App ${showDarkClass}`}>
+                    <Navbar
+                        handleDarkMode={handleDarkMode}
+                        showDark={showDark}
+                        showDarkClass={showDarkClass}
+                    />
+                    <ToastContainer autoClose={2500} />
+                    <Outlet />
+                </div>
+            </body>
+        </>
+    );
+}
+
+export default App;
